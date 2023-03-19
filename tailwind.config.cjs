d3ed0278898c0daf3a67a029936bc3c4e82eef28
Jafar Aziz <aziz@jafaraziz.com>
2023-03-19 12:20:05 +0700

    🚀 website - update package and update code style in tailwind prose

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,25 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			typography: ({ theme }) => ({
+				DEFAULT: {
+					css: {
+						'code': {
+							'&:after': {
+                display: 'none',
+              },
+							'&:before': {
+                display: 'none',
+              },
+							'background-color': theme('colors.gray[200]'),
+							'padding': `0 ${theme('spacing[1]')}`,
+							'border-radius': theme("borderRadius.sm")
+						},
+					}
+				}
+			})
+		},
 		fontFamily: {
 			'sans': ['Plus Jakarta Sans', 'sans-serif'],
 			'mono': ['JetBrains Mono', 'monospace']
