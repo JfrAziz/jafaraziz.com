cdb0c026555407ae949d10e6edc16330456cd8b3
Jafar Aziz <aziz@jafaraziz.com>
2023-04-19 21:13:25 +0700

    🚀 website - add line numbers to shiki code code highlighter from tailwind config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -8,14 +8,26 @@ module.exports = {
 					css: {
 						'code': {
 							'&:after': {
-                display: 'none',
-              },
+								display: 'none',
+							},
 							'&:before': {
-                display: 'none',
-              },
+								display: 'none',
+							},
+							'> .line::before': {
+								'content': 'counter(step)',
+								'counter-increment': 'step',
+								'margin-right': theme('spacing[4]'),
+								'display': 'inline-block',
+								'width': '1rem',
+								'font-size': '0.75rem',
+								'text-align': 'right',
+								'color': theme('colors.gray[300]')
+							},
 							'background-color': theme('colors.gray[200]'),
 							'padding': `0 ${theme('spacing[1]')}`,
-							'border-radius': theme("borderRadius.sm")
+							'border-radius': theme("borderRadius.sm"),
+							'counter-reset': 'step',
+							'counter-increment': 'step 0',
 						},
 					}
 				}
