e58e7f3e1ec257a8cbee77f69a75b89edea370bc
Jafar Aziz <aziz@jafaraziz.com>
2026-01-02 00:22:42 +0700

    feat(website): setup cloudflare deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,12 @@ import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
+import cloudflare from "@astrojs/cloudflare";
 
 export default defineConfig({
   integrations: [react(), mdx()],
   vite: {
     plugins: [tailwindcss()],
   },
+  adapter: cloudflare(),
 });
