cdc7e2ad93db197748ea1d6a02a704b3d346677c
Jafar Aziz <aziz@jafaraziz.com>
2026-01-02 00:46:35 +0700

    fix(website): fix build url became localhost with adding site url in config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,11 @@ import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
-import cloudflare from "@astrojs/cloudflare";
 
 export default defineConfig({
+  site: "https://jafaraziz.com",
   integrations: [react(), mdx()],
   vite: {
     plugins: [tailwindcss()],
   },
-  adapter: cloudflare(),
 });
